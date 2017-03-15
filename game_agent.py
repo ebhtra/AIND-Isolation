0f57ee242144a33ddc3fb35ee7c5e5b1cf032f9d
Ethan Haley <ebhtra@gmail.com>
2017-03-15 04:19:55 -0400

    Clean up and improve comments.

--- game_agent.py
+++ game_agent.py
@@ -6,9 +6,6 @@ augment the test suite with your own test cases to further test your code.
 You must test your agent's strength against a set of agents with known
 relative strength using tournament.py and include the results in your report.
 """
-#import random
-#import isolation
-
 
 class Timeout(Exception):
     """Subclass base exception for code clarity."""
@@ -37,22 +34,67 @@ def custom_score(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
+    # First check if game is over after this move
+    if game.is_winner(player):
+        return float("inf")
+    # These two game-over checks are in the wrong order in the sample players.
+    # Since the game state being scored here is one in which the move has been
+    #   forecast already, the next player to move is not the player parameter
+    #   here.  So first the opponent must be given the chance to lose, which
+    #   translates to checking if game.is_winner(player) first. 
     if game.is_loser(player):
         return float("-inf")
 
-    if game.is_winner(player):
-        return float("inf")
+    # What moves does a player have from a given position?
+    #  First turn:
+    branches = set(game.get_legal_moves(player))
+    #  Second turn:
+    twigs = set(sum([game.__get_moves__(branch) for branch in branches],[]))
+
+    # What moves does an adversary have from a given position?
+    #  First turn:
+    enemy_branches = set(game.get_legal_moves(game.get_opponent(player)))
+    #  Second turn:
+    enemy_twigs = set(sum([game.__get_moves__(branch) for 
+                           branch in enemy_branches],[]))
     
-    cx, cy = game.width / 2, game.height / 2
-    dx = game.get_player_location(player)[1] + 0.5 - cx
-    dy = game.get_player_location(player)[0] + 0.5 - cy
-    open_moves = len(game.get_legal_moves(player))
-    open_enemy = len(game.get_legal_moves(game.get_opponent(player)))
-    if game.move_count < game.width * game.height / 3.5:
-        return -(dx ** 2) -(dy ** 2)
-    else:
-        return 1.3 * open_moves - open_enemy
-                                 
+    # Score based on how many unstoppable moves player has in ensuing two
+    #    turns, vs. same for opponent.
+    #
+    # The order of turns after the current moves being scored by self are:
+    #        1) enemy_branches
+    #        2) branches (for self)
+    #        3) enemy_twigs
+    #        4) twigs (for self) 
+    # Any good future moves for either player that can first be taken by
+    #    the opponent must be subtracted from player's score.
+    return float(  len(twigs - enemy_twigs - enemy_branches)
+                 + len(branches - enemy_branches)
+                 - len(enemy_branches)
+                 - len(enemy_twigs - branches)  )
+    
+#==============================================================================
+#     Unused heuristics follow
+#==============================================================================
+
+#    # For the first fraction of the game use the central heuristic
+#    cx, cy = game.width / 2, game.height / 2   # The centerpoint of the board
+#    # How far off-center is a location? 
+#    dx = game.get_player_location(player)[1] + 0.5 - cx
+#    dy = game.get_player_location(player)[0] + 0.5 - cy
+
+#    if game.move_count < game.width * game.height / 4.0 :
+#        return -(dx ** 2) -(dy ** 2)  # penalize Euclidean distance from center
+#    else:
+#        # Use a slightly weighted version of "improved" heuristic
+#        return 1.3 * len(branches) - len(enemy_branches)
+
+
+#     # Alternating weights of "improved" heuristic 
+#    if game.move_count // 2 % 2 == 0:  # Toggle every 2 plies
+#        return 2.0 * len(branches) - len(enemy_branches)                
+#    else:
+#        return len(branches) - 2.0 * len(enemy_branches)
 
 
 class CustomPlayer:
@@ -132,8 +174,9 @@ class CustomPlayer:
 
         self.time_left = time_left
         
-        if not legal_moves:  
-            return (-1,-1)  # game over Sentinel
+        if not legal_moves: 
+            # game is over
+            return (-1,-1)  # game over sentinel
             
         # Get a value to return in case early timeout.
         # This var will store the best move after each deeper iteration.
@@ -142,11 +185,12 @@ class CustomPlayer:
         # Map string arguments to the appropriate function calls
         methods = {'minimax': self.minimax, 'alphabeta': self.alphabeta}
         
-        # 'unchanged' will be used as a counter to gauge quiescence in order
-        #      to abort a search that seems to have run its course
+        # 'unchanged' will be used as a counter to gauge when to
+        #      abort a search that seems to have run its course.
         unchanged = 0
-        
+        # 'depth' will tell each successive iteration how far to go
         depth = 0
+        
         # TODO: Perform any required initializations, including selecting an initial
         # move from the game board (i.e., an opening book)
 
@@ -165,6 +209,7 @@ class CustomPlayer:
                     # Use the recursive search function to predict the score
                     #     for this branch/move.
                     score, _ = f(new_board, depth, maximizing_player=False)
+                    
                     if score == float('inf'):  # This move is a game winner.
                         return move
                     elif score == -float('inf'): # This move is a game loser.
@@ -178,25 +223,29 @@ class CustomPlayer:
                     else:
                         new_scores.append((score, move))
                 if not new_scores:
+                    # Take the best move from previous depth if all new fail
                     return last_best[-1]
+                
                 new_best = max(new_scores)
                 if new_best[0] != last_best[0]:
+                    # If the best score for new depth is different from
+                    #   best for previous depth, reset unchanged counter.
                     unchanged = 0
+                # Update the best move in case loop terminates or times out
                 last_best = new_best
+                # Get ready to search one move deeper into future games
                 depth += 1
-                
+            # If score hasn't changed for several depths or if iterative is
+            #    set False and search depth has reached max passed in, return
+            #    best move when while loop terminates.
             return last_best[-1]
-            # The search method call (alpha beta or minimax) should happen in
-            # here in order to avoid timeout. The try/except block will
-            # automatically catch the exception raised by the search method
-            # when the timer gets close to expiring
             
 
         except Timeout:
             # Handle any actions required at timeout, if necessary
             return last_best[-1]
+        
         # Return the best move from the last completed search iteration
-#        
         return last_best[-1]
 
     def minimax(self, game, depth, maximizing_player=True):
@@ -249,12 +298,15 @@ class CustomPlayer:
             return game.utility(boss), (-1,-1)
         
         choices = [game.forecast_move(move) for move in moves]
+        # Now call this method recursively, swapping maximizing_player
         scores = [self.minimax(choice, depth-1, not maximizing_player)[0]
                           for choice in choices]
         
         if maximizing_player:
+            # Select the move with the highest minimized score
             return max(zip(scores, moves))
         else:
+            # Select the move with the lowest maximized score
             return min(zip(scores, moves))
             
 
@@ -303,41 +355,65 @@ class CustomPlayer:
         #    agent can calculate utility of moves correctly
         if maximizing_player:
             boss = game.active_player
-            #best_score = alpha
-            best_score = -float('inf')
-        else:
+            # Init the best score that will be returned eventually
+            best_score = alpha
+            # The textbook initializes as below instead, but I believe that's
+            #    less efficient than above.
+            #best_score = -float('inf')
+        else:  # (minimizing player is active player)
             boss = game.inactive_player
-            #best_score = beta
-            best_score = float('inf')
+            best_score = beta
+            # Textbook:
+            #best_score = float('inf')
             
         if depth == 0:
+            # Base case for recursion
             return self.score(game, boss), None
             
         moves = game.get_legal_moves()
         if not moves:
+            # game would end here, so return +inf or -inf for score
             return game.utility(boss), (-1,-1)
         
-        best_move = (-1,-1)
+        best_move = (-1,-1)  # Initialize move than can be returned with score
         
         for move in moves:
+            
             board = game.forecast_move(move)
+            # Call this method recursively, swapping maximizing_player
             score, _ = self.alphabeta(board, depth-1, alpha, beta,
                                    not maximizing_player)
             
             if maximizing_player:
                 if score >= beta:
+                    # Smallest score opponent could stick this player with on
+                    #   this branch is larger than a score that opponent can
+                    #   stick this player with on a different branch, so return
+                    #   now to not waste time here.
                     return score, move
                 elif score > alpha:
+                    # Smallest score allowed by opponent is better than what
+                    #   this player has scored elsewhere, so make this the
+                    #   new benchmark.
                     alpha, best_score, best_move = score, score, move
-                elif score > best_score:
-                    best_score, best_move = score, move
-            else:
+                # Only need following if initializing best_score as in textbook.
+#                elif score > best_score:
+#                    best_score, best_move = score, move
+            else:  # (minimizing layer)
                 if score <= alpha:
+                    # Highest score this player gets here is worse
+                    #   than what he can get on a different branch, so return
+                    #   now in order to prune (not waste time on) the rest of
+                    #   this branch, which opponent is minimizing.
                     return score, move
                 elif score < beta:
+                    # Highest score found by this player is lower than on all
+                    #   other branches so far explored, so this score becomes
+                    #   the new benchmark for the minimizing player 
                     beta, best_score, best_move = score, score, move
-                elif score < best_score:
-                    best_score, best_move = score, move
+                # Only need following if initializing best_score as in textbook.
+#                elif score < best_score:
+#                    best_score, best_move = score, move
         return best_score, best_move
         
             
